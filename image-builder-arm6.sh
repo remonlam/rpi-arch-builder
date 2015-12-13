c5428d095c5babbe3c489acbf8ed268272fb2358
Remon Lam <remon.lam@lamnet.nl>
2015-12-13 16:38:51 +0100

    fixes

--- image-builder-arm6.sh
+++ image-builder-arm6.sh
@@ -14,10 +14,15 @@ fi
 echo "PI 1 MODEL A+, PI 1 MODEL B+, PI ZERO are V6 --- PI 2 MODEL B is V7"
 read -p 'What version of Pi? v6 or v7 ' armversion
 read -p 'Enter device name (SD-Card): like sdb: ' sdCard
+read -p 'Enter a new hostname: ' hostName
 read -p 'Enter wifi name (Accesspoint): ' wifiAP
 read -p 'Enter wifi password: ' wifiKey
 part1=1
 part2=2
+
+# Wipe microSD card @ $sdCard
+#echo "Wipe microSD card ('$sdCard')"
+#dd if=/dev/zero of=/dev/$sdCard bs=1M count=1
 
 # Do some pre-requirements
 # Install wget and badtar
@@ -48,7 +53,7 @@ mount /dev/$sdCard$part2 root
 echo "Download Arch Linux ARM v'$armversion' and expand to root"
   if [ $armversion=6 ]; then
     echo "Downloading Arch Linux ARM v'$armversion'"
-     wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
+    # wget http://archlinuxarm.org/os/ArchLinuxARM-rpi-latest.tar.gz
     echo "Download complete, expanding tar.gz to root"
      bsdtar -xpf ArchLinuxARM-rpi-latest.tar.gz -C root
      sync
@@ -71,19 +76,44 @@ mv /tmp/configure-system.sh root
 
 # Download Wi-Fi files from GitHub
 ### TODO: CHECK PATHS TO MOUNT POINT!!
-wget -P root/etc/systemd/network/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/wlan0.network
-wget -P root/etc/systemd/system/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/wpa_supplicant.service
-wget -P root/etc/wpa_supplicant/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/wlan0.conf
+#wget -P root/etc/systemd/network/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/wlan0.network
+#wget -P root/etc/systemd/system/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/wpa_supplicant.service
+#wget -P root/etc/wpa_supplicant/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/wlan0.conf
 # NOTE: NEES TO BE CHECKED IF WOKRING
-sleep 2
-sed -i 's/AccessPointName/'$wifiAP'/' root/etc/wpa_supplicant/wlan0.conf
-sed -i 's/TopSecretPassword/'$wifiKey'/' root/etc/wpa_supplicant/wlan0.conf
+#sleep 2
+#sed -i 's/TopSecretPassword/'$wifiKey'/' root/etc/wpa_supplicant/wlan0.conf
+#sed -i 's/AccessPointName/'"$wifiAP"'/' root/etc/wpa_supplicant/wlan0.conf
+
+
+
+
+# Copy extra sources
+cp -rf /root/Desktop/extra_sources/* root/
+
+# Copy netctl wlan0 config file
+cp -rf /root/Desktop/wlan0 root/etc/netctl/
+# Replace SSID name
+sed -i "s/ESSID='SSID-NAME'/ESSID='$wifiAP'/" root/etc/netctl/wlan0
+# Replace SSID password
+sed -i "s/Key='SSID-KEY'/Key='$wifiKey'/" root/etc/netctl/wlan0
+
+
+# Copy wlan0.service file to systemd and create symlink to make it work at first boot
+cp -rf /root/Desktop/symlink/netctl@wlan0.service root/etc/systemd/system/
+ln -s 'root/etc/systemd/system/netctl@wlan0.service' 'root/etc/systemd/system/multi-user.target.wants/netctl@wlan0.service'
+
+# Enable root logins for sshd
+sed -i "s/"#"PermitRootLogin prohibit-password/PermitRootLogin yes/" root/etc/ssh/sshd_config
+# Change hostname
+sed -i 's/alarmpi/'$hostName'/' root/etc/hostname
+
+
 
 # Do a final sync, and wait 5 seconds before unmouting
 sync
 echo "Wait 5 seconds before unmouting 'boot' and 'root' mount points"
 sleep 5
 
-#Unmount the two partitions:
-umount boot root
+#Unmount the boot and root partitions:
+#umount boot root
 echo "Unmount completed, it's safe to remove the microSD card!"
