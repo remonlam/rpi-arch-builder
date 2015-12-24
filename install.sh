fe62806b624743620139b60e8b5c1765df011fd4
Remon Lam <remon.lam@lamnet.nl>
2015-12-24 09:44:53 +0100

    push

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi" >> /etc/apt/sources.list
+
+aptget update /
+apt-get install -y nano /
+                wget /
+                ssh /
+                python /
+                python3
+
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/python3-rtimulib_7.2.1-3_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/librtimulib-dev_7.2.1-3_armhf.deb /
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/librtimulib-utils_7.2.1-3_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/librtimulib7_7.2.1-3_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/libwebpdemux1_0.4.1-1.2_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/libwebpmux1_0.4.1-1.2_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/python-pil_2.6.1-2_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/python-rtimulib_7.2.1-3_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/python-sense-hat_2.1.0-1_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/python3-pil_2.6.1-2_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/python3-rtimulib_7.2.1-3_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/python3-sense-hat_2.1.0-1_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/raspberrypi-bootloader_1.20151118-1_armhf.deb
+wget -P /tmp https://github.com/remonlam/rpi-docker-weather/raw/master/packages/sense-hat_1.2_all.deb
