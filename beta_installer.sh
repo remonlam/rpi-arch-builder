04b2410fec189f91ec850e7833bddb15ecc9cbff
Remon Lam <remon.lam@lamnet.nl>
2016-04-25 11:53:56 +0200

    update

--- beta_installer.sh
+++ beta_installer.sh
@@ -17,8 +17,11 @@ echo "NOTE: PI 1 MODEL A+, PI 1 MODEL B+, PI ZERO are 6 --- PI 2 MODEL B is 7"
 read -p 'What version of Pi? 6 or 7 ' armVersion
 read -p 'Enter device name (SD-Card): like sdb: ' sdCard
 read -p 'Enter a new hostname: ' hostName
-read -p 'Enter wifi name (Accesspoint): ' wifiAP
-read -p 'Enter wifi password: ' wifiKey
+
+## THIS COULD BE REMOVED WHEN USING THE NEW RUNTIME
+#read -p 'Enter wifi name (Accesspoint): ' wifiAP
+#read -p 'Enter wifi password: ' wifiKey
+
 part1=1
 part2=2
 
