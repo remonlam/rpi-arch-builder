71f33817a6bac85c152be0f0a5df4c02a2331c0e
Remon Lam <remon.lam@lamnet.nl>
2016-04-25 11:48:53 +0200

    update

--- runtime.sh
+++ runtime.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 # Set vars
-read -p 'Select network type: wifi/cable/both: ' networkType
+read -p 'Select network type: wifi/ethernet/both: ' networkType
 
 
 # Check what type of networking is used
+echo "##############################################################"
 echo "Check what network type is used: '$networkType'"
+echo "##############################################################"
 if [ $networkType = "wifi" ]; then
   echo "Using Wi-Fi networking"
     read -p 'Enter wifi interface: example; wlan0 ' wifiInterface
     read -p 'Enter wifi name (Accesspoint): ' wifiAP
     read -p 'Enter wifi password: ' wifiKey
     read -p 'Using DHCP or Fixed IP: DHCP/FIXED ' wifiIpType
-elif [ $networkType = "cable" ]; then
-  echo "Using Ethernet/cable networking"
+elif [ $networkType = "ethernet" ]; then
+  echo "Using Ethernet networking"
     read -p 'Enter ethernet interface: example; eth0 ' ethernetInterface
     read -p 'Using DHCP or Fixed IP: DHCP/FIXED ' ethernetIpType
 elif [ $networkType = "both" ]; then
@@ -21,6 +23,7 @@ elif [ $networkType = "both" ]; then
     read -p 'Enter wifi interface: example; wlan0 ' wifiInterface
     read -p 'Enter wifi name (Accesspoint): ' wifiAP
     read -p 'Enter wifi password: ' wifiKey
+    read -p 'Using DHCP or Fixed IP: DHCP/FIXED ' wifiIpType
     read -p 'Enter ethernet interface: example; eth0 ' ethernetInterface
 else
    echo "'$networkType' = Invalid variable: ....Go home your drunk...."
@@ -29,10 +32,16 @@ fi
 echo "###########################################"
 echo "Network selection has been set correctly..."
 echo "###########################################"
+echo ""
+echo ""
 
 
 # Check if DHCP or FIXED IP needs to be configured
+echo "##############################################################"
 echo "Check if DHCP or FIXED IP needs to be configured"
+echo "**************************************************************"
+echo "This let's you choose between Wi-Fi, Ethernet or both of them"
+echo "##############################################################"
 if [ "$wifiIpType" = "FIXED" ]; then
   echo "Setup Fixed IP settings for: '$networkType'"
     read -p 'Enter IP Address: ' networkWifiIP
@@ -63,8 +72,7 @@ elif [ "$networkType" = "both" ]; then
     read -p 'Enter DNS 1: ' networkEthernetDns1
     read -p 'Enter DNS 2: ' networkEthernetDns2
 else
-  echo "'$networkType' = Invalid variable: ....Go home your drunk...."
-    exit 1
+  echo "You're selected DHCP so no IP settings needs to be configured"
 fi
 echo "###########################################"
 echo "IP type selection has been set correctly..."
