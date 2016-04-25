3fb693feb7ac84d9bdcc8651533d7679a82882f0
Remon Lam <remon.lam@lamnet.nl>
2016-04-25 10:58:51 +0200

    update

--- runtime.sh
+++ runtime.sh
@@ -8,15 +8,64 @@ read -p 'Select network type: wifi/cable/both: ' networkType
 echo "Check what network type is used: '$networkType'"
 if [ $networkType = "wifi" ]; then
   echo "Using Wi-Fi networking"
-   ping google.nl
+    read -p 'Enter wifi interface: example; wlan0 ' wifiInterface
+    read -p 'Enter wifi name (Accesspoint): ' wifiAP
+    read -p 'Enter wifi password: ' wifiKey
+    read -p 'Using DHCP or Fixed IP: DHCP/FIXED ' wifiIpType
 elif [ $networkType = "cable" ]; then
   echo "Using Ethernet/cable networking"
-   ping containerstack.io
+    read -p 'Enter ethernet interface: example; eth0 ' ethernetInterface
+    read -p 'Using DHCP or Fixed IP: DHCP/FIXED ' ethernetIpType
 elif [ $networkType = "both" ]; then
  echo "Using both Wi-Fi and Ethernet networking"
-  ping nu.nl
+    read -p 'Enter wifi interface: example; wlan0 ' wifiInterface
+    read -p 'Enter wifi name (Accesspoint): ' wifiAP
+    read -p 'Enter wifi password: ' wifiKey
+    read -p 'Enter ethernet interface: example; eth0 ' ethernetInterface
 else
-   echo "'$networkType'= Invalid variable: ....Go home your drunk.... :-)"
+   echo "'$networkType' = Invalid variable: ....Go home your drunk...."
    exit 1
 fi
-echo "Go home your drunk.. :-)"
+echo "###########################################"
+echo "Network selection has been set correctly..."
+echo "###########################################"
+
+
+# Check if DHCP or FIXED IP needs to be configured
+echo "Check if DHCP or FIXED IP needs to be configured"
+if [ "$wifiIpType" = "FIXED" ]; then
+  echo "Setup Fixed IP settings for: '$networkType'"
+    read -p 'Enter IP Address: ' networkWifiIP
+    read -p 'Enter IP Subnet: ' networkWifiSubnet
+    read -p 'Enter Gateway: ' networkWifiGateway
+    read -p 'Enter DNS 1: ' networkWifiDns1
+    read -p 'Enter DNS 2: ' networkWifiDns2
+elif [ "$ethernetIpType" = "FIXED" ]; then
+  echo "Setup Fixed IP settings for: '$networkType'"
+    read -p 'Enter IP Address: ' networkEthernetIP
+    read -p 'Enter IP Subnet: ' networkEthernetSubnet
+    read -p 'Enter Gateway: ' networkEthernetGateway
+    read -p 'Enter DNS 1: ' networkEthernetDns1
+    read -p 'Enter DNS 2: ' networkEthernetDns2
+elif [ "$networkType" = "both" ]; then
+  echo "Setup Fixed IP settings for: Wi-Fi"
+  echo "###########################################"
+    read -p 'Enter IP Address: ' networkWifiIP
+    read -p 'Enter IP Subnet: ' networkWifiSubnet
+    read -p 'Enter Gateway: ' networkWifiGateway
+    read -p 'Enter DNS 1: ' networkWifiDns1
+    read -p 'Enter DNS 2: ' networkWifiDns2
+  echo "Setup Fixed IP settings for: Ethernet"
+  echo "###########################################"
+    read -p 'Enter IP Address: ' networkEthernetIP
+    read -p 'Enter IP Subnet: ' networkEthernetSubnet
+    read -p 'Enter Gateway: ' networkEthernetGateway
+    read -p 'Enter DNS 1: ' networkEthernetDns1
+    read -p 'Enter DNS 2: ' networkEthernetDns2
+else
+  echo "'$networkType' = Invalid variable: ....Go home your drunk...."
+    exit 1
+fi
+echo "###########################################"
+echo "IP type selection has been set correctly..."
+echo "###########################################"
