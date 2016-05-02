5d8b73e69c15a7ab3085e47d7f4bce3b014bbedc
Remon Lam <remon.lam@lamnet.nl>
2016-05-02 13:51:54 +0200

    update

--- testing/remove_part.sh
+++ testing/remove_part.sh
@@ -1,5 +1,5 @@
 # Remove each partition
 for v_partition in $(parted -s /dev/sdb print|awk '/^ / {print $1}')
 do
-   parted -s /dev/sda rm ${v_partition}
+   parted -s /dev/sdb rm ${v_partition}
 done
