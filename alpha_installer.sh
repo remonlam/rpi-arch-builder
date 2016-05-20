c334ebf3c153bfcdf8d5fb17d1f852a1c13b309b
Remon Lam <remon.lam@lamnet.nl>
2016-05-20 23:30:39 +0200

    added systemPreConfiguration function

--- alpha_installer.sh
+++ alpha_installer.sh
@@ -200,6 +200,31 @@ function selectArmVersion {
 #########################################################################################
 
 
+function systemPreConfiguration {
+# System configuration
+  echo "#########################################################################"
+  echo "System pre-configuration"
+  echo "#########################################################################"
+# Change rotation of Pi Screen' >> /temp/boot/config.txt
+  echo lcd_rotate=2 >> /temp/boot/config.txt
+# Change GPU memory from 64MB to 16MB
+  sed -i 's/gpu_mem=64/gpu_mem=16/' /temp/boot/config.txt
+# Enable root logins for sshd
+  sed -i "s/"#"PermitRootLogin prohibit-password/PermitRootLogin yes/" /temp/root/etc/ssh/sshd_config
+# Change hostname
+  sed -i 's/alarmpi/'$varHostName'/' /temp/root/etc/hostname
+# Download post configuration script and make file executable
+  {
+  wget -P /temp/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/systemd_config/configure-system.sh
+  chmod 755 /temp/configure-system.sh
+  } &> /dev/null
+# Copy "configure-system.sh" script to "root"
+  mv /temp/configure-system.sh /temp/root
+  echo "#########################################################################"
+  echo ""
+  echo ""
+}
+
 
 ###################################
 ## Network functions
