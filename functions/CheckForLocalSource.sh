f3a384a11525be482519159910d630103604f614
Remon Lam <remon.lam@lamnet.nl>
2016-06-24 15:38:42 -0400

    added md5 check

--- functions/CheckForLocalSource.sh
+++ functions/CheckForLocalSource.sh
@@ -4,9 +4,21 @@ if [ -f "$FILE" ];
 then
    echo "File $FILE exist."
    varCheckForLocalSource="true"
+   varTest="md5 -q test.txt"
 else
    echo "File $FILE does not exist" >&2
    varCheckForLocalSource="false"
 fi
 echo $varCheckForLocalSource
+echo $FILE
+echo $1
+# To Do;
+# - Add check based on md5 file hash
+# - make a if file exists than do md5 check
+
+## know hash of test.txt
+# 80e8ed47c871c8df9a1a0b9610dc7a52
+
+md5 -q test.txt
+
 }
