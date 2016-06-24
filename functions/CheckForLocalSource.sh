982ddda93d2d5fbcf0069fe4d732a8fba0688558
Remon Lam <remon.lam@lamnet.nl>
2016-06-24 18:49:33 -0400

    disabled functions

--- functions/CheckForLocalSource.sh
+++ functions/CheckForLocalSource.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-function CheckForLocalSource {
+
+# Create function call disabled for now.
+#function CheckForLocalSource {
+###
+
 if [ -f "$FILE" ];
 then
    echo "File $FILE exist."
@@ -15,13 +19,15 @@ echo $FILE
 echo $1
 
 
+if [[ $(md5sum "$FILE") = 5d40f31729c992b5a0e67490689fe8ff* ]]
+
+
+
 # To Do;
 # - Add check based on md5 file hash
 # - make a if file exists than do md5 check
 
 ## know hash of test.txt
 # 80e8ed47c871c8df9a1a0b9610dc7a52
-
-md5 -q test.txt
-
-}
+#md5 -q test.txt
+# Disabled function call on line3}
