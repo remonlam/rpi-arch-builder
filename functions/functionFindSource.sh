dfd47a71b330b5c1fd1c486dd369392a79295d43
Remon Lam <remon.lam@lamnet.nl>
2016-07-18 13:26:13 +0200

    update function

--- functions/functionFindSource.sh
+++ functions/functionFindSource.sh
@@ -17,20 +17,9 @@ echo $varCheckForLocalSource
 echo $FILE
 echo $1
 
-
 if [[ $(md5sum "test.txt") = d41d8cd98f00b204e9800998ecf8427e* ]]
 md5sum -c "d41d8cd98f00b204e9800998ecf8427e" "test.txt"
 
 if [[ $(md5sum "$test") = d41d8cd98f00b204e9800998ecf8427e* ]]
 
-
-
-# To Do;
-# - Add check based on md5 file hash
-# - make a if file exists than do md5 check
-
-## know hash of test.txt
-# 80e8ed47c871c8df9a1a0b9610dc7a52
-#md5 -q test.txt
-# Disabled function call on line3
 }
