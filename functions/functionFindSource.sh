62d7dfd46240b012cb73fed6c1567245718ee24b
Remon Lam <remon.lam@lamnet.nl>
2016-07-18 13:25:16 +0200

    fix function

--- functions/functionFindSource.sh
+++ functions/functionFindSource.sh
@@ -2,6 +2,7 @@
 
 # Create function call disabled for now.
 function functionFindSource {
+
 if [ -f "$FILE" ];
 then
    echo "File $FILE exist."
@@ -31,5 +32,5 @@ if [[ $(md5sum "$test") = d41d8cd98f00b204e9800998ecf8427e* ]]
 ## know hash of test.txt
 # 80e8ed47c871c8df9a1a0b9610dc7a52
 #md5 -q test.txt
-# Disabled function call on line3}
+# Disabled function call on line3
 }
