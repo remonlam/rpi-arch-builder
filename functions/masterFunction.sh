2d0e28222c06cfd42b8adcc3837721b3b11898b8
Remon Lam <remon.lam@lamnet.nl>
2016-07-18 10:08:50 +0200

    add functions

--- functions/masterFunction.sh
+++ functions/masterFunction.sh
@@ -4,6 +4,6 @@
 ## this script will call all functions in this directory
 
 ## This is just a master test script it wont be used for something :-)
-. ./functions/testImportFunction.sh
-. ./functions/testImportFunction0.sh
-. ./functions/testImportFunction00.sh
+. ./functions/masterVariables.sh
+. ./functions/setRootPassword.sh
+. ./functions/formatDisk.sh
---
+++ functions/masterVariables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Set fixed variables
+part1=1
+part2=2
+
+# Set fixed variables for mounting sd card
+mount_boot=/temp/boot
+mount_root=/temp/root
