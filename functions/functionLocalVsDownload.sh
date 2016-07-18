a8268108d4b004cab8fcbd05f4a3d9dca472b6c0
Remon Lam <remon.lam@lamnet.nl>
2016-07-18 15:07:18 +0200

    update LocalImage

--- functions/functionLocalVsDownload.sh
+++ functions/functionLocalVsDownload.sh
@@ -4,8 +4,16 @@ function functionLocalVsDownload {
 if [ "$varCheckForLocalSource" = "TRUE" ]; then
   echo "Using local ISO source, no download required..."
   echo "#########################################################################"
-  #functionSelectArmVersion
-  echo "fake exec of arm_function"
+  echo "Download complete, expanding image to root"
+   {
+   bsdtar -xpf $FILE -C /temp/root
+   sync
+   } &> /dev/null
+  #Move boot files to the first partition:
+    mv /temp/root/boot/* /temp/boot
+    echo "#########################################################################"
+    echo ""
+    echo ""
     elif [ "$varCheckForLocalSource" = "FALSE" ]; then
         echo "No local ISO source found, download is required :-( "
         echo "#########################################################################"
