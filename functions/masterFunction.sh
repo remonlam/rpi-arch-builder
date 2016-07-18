c5bfb94f88028c7d8c5f89f09aca557bff6928e1
Remon Lam <remon.lam@lamnet.nl>
2016-07-18 10:02:41 +0200

    Revert "move to external functions"
    This reverts commit bba1716f58d9ff280bc35d487210f57e42fec569.

--- functions/masterFunction.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Master function script
-## this script will call all functions in this directory
-
-## This is just a master test script it wont be used for something :-)
-. ./functions/masterVariables.sh
-. ./functions/setRootPassword.sh
-. ./functions/formatDisk.sh
