547089122ffde609379d371babf023a55a58e594
Remon Lam <remon.lam@lamnet.nl>
2016-08-07 22:25:53 +0200

    disable systemctl function

--- installer.sh
+++ installer.sh
@@ -13,7 +13,7 @@ functionRootCheck
 functionFormatDisk
 functionFindSource
 #functionSelectArmVersion
-#functionDisableSystemctlServices #need to chec if this is the right place to execute this function
+#functionDisableSystemctlServices #need to check if this is the right place to execute this function
 
 functionsNetworkProfileSelection
 functionSetRootPassword
