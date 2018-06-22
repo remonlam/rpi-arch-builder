87ee703943a52148fdd5e65cd78412b10bd005b3
Remon Lam <remon@containerstack.io>
2018-06-22 15:30:53 +0200

    migrate to systemd for networking

--- functions/functionNetIpEthernetStatic.sh
+++ functions/functionNetIpEthernetStatic.sh
@@ -21,19 +21,21 @@ function functionNetIpEthernetStatic {
     wget -P /temp/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/networking/eth0
     cp -rf /temp/eth0 /temp/root/etc/netctl/
   # Copy wlan0.service file to systemd and create symlink to make it work at first boot
-    wget -P /temp/ https://raw.githubusercontent.com/remonlam/rpi-zero-arch/master/networking/netctl%40eth0.service
+    cp ../sources/eth0.network /temp/etc/systemd/network/
     cp -rf /temp/netctl@eth0.service /temp/root/etc/systemd/system/
     ln -s '/temp/root/etc/systemd/system/netctl@eth0.service' '/temp/root/etc/systemd/system/multi-user.target.wants/netctl@eth0.service'
   } &> /dev/null
 
 # Prepping Ethernet configuration files
   echo "Prepping Ethernet netctl config files"
-    sed -i "s/IP=dhcp/IP=static/" /temp/root/etc/netctl/eth0
-    sed -i "/IP=static/ a Address=('$ethernetIp/$ethernetMask')" /temp/root/etc/netctl/eth0
-    sed -i "/Address=/ a Gateway=('$ethernetGateway')" /temp/root/etc/netctl/eth0
-    sed -i "/Gateway=/ a DNS=('$ethernetDns1' '$ethernetDns2')" /temp/root/etc/netctl/eth0
+    sed -i "s/IP=dhcp/IP=static/" /temp/root/etc/systemd/network/eth0.network
+    sed -i "/IP=static/ a Address=('$ethernetIp/$ethernetMask')" /temp/root/etc/systemd/network/eth0.network
+    sed -i "/Address=/ a Gateway=('$ethernetGateway')" /temp/root/etc/systemd/network/eth0.network
+    sed -i "/Gateway=/ a DNS=('$ethernetDns1' '$ethernetDns2')" /temp/root/etc/systemd/network/eth0.network
 
  # Remove "systemd-networkd.service"
     rm -rf /temp/root/etc/systemd/system/multi-user.target.wants/systemd-networkd.service
     rm -rf /temp/root/etc/systemd/system/socket.target.wants/systemd-networkd.service
 }
+
+/etc/systemd/network/eth0.network
